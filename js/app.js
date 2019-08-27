8b09bdd2c555e83cc3ca31fa1902cd831d0a4f8b
H Brown <hebrown12@gmail.com>
2019-08-26 20:41:11 -0500

    almost completed html/css; adding jquery

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//     // do stuff
+//     console.log('Hello World');
+//     $().click(function(){
+//         // do stuff
+//         // when I click on project image, I want image in project movie
+//         $('.hello').toggleClass('goodbye');
+//     })
+// });
+
+.$(document).click(function() {
+    $('#toggle').slideToggle(200);
+})
